bb0abfb429ac65cad8c28567cbc69027beeddf05
skald1311 <hoangmanhduong01@gmail.com>
2024-08-06 16:26:28 -0600

    data: add test files

---
+++ utilities/create_data_tables.py
@@ -0,0 +1,9 @@
+def main():
+    target_file = "JA_FPKM.csv"
+    cluster_file = "JA_FPKM_means.csv"
+    tf_file = "Arabidopsis_TFs_AGRIS.xlsx"
+    candidates_file = "SC-ION_candidates.txt"
+    
+
+if __name__ == "__main__":
+    main()
